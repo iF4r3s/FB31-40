804b6bcad00feb9f513d811a0799860f33618f37
iF4r3s <44950873+iF4r3s@users.noreply.github.com>
2018-12-07 13:06:47 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,358 @@
+const Discord = require('discord.js');
+const bot31 = new Discord.Client();
+const bot32 = new Discord.Client();
+const bot33 = new Discord.Client();
+const bot34 = new Discord.Client();
+const bot35 = new Discord.Client();
+const bot36 = new Discord.Client();
+const bot37 = new Discord.Client();
+const bot38 = new Discord.Client();
+const bot39 = new Discord.Client();
+const bot40 = new Discord.Client();
+
+
+bot31.on('ready', () => {
+    console.log(`S31-30`)
+});
+
+
+bot31.on('message', message => {
+    if(message.content.startsWith(`f-d`)){
+        message.channel.send(`#daily`);
+    }
+});
+
+
+bot31.on('message', message => {
+    if(message.content.startsWith(`f-r`)){
+        message.channel.send(`#rep <@505101712839671838>`)
+    }
+});
+
+
+bot31.on('ready', () => {
+    let channel = bot31.guilds.get("520354655604178958").channels.find(c => c.id == "520370626595258392");
+    setInterval(function(){
+        channel.send(`FB31 Spammer`)
+    }, 2000);
+});
+
+
+bot31.on('message', message => {
+    if(message.author.bot) return;
+    let command = message.content.split(" ")[0];
+    let args = message.content.split(" ").slice(1);
+    if(!args) return message.reply(`**شو بدك امي تقول؟؟؟**`);
+    if(command == "f31"){
+        message.channel.send(args.join(" "))
+    }
+});
+
+////////////////////////////////////////////////////////////////
+bot32.on('message', message => {
+    if(message.content.startsWith(`f-d`)){
+        message.channel.send(`#daily`);
+    }
+});
+
+
+bot32.on('message', message => {
+    if(message.content.startsWith(`f-r`)){
+        message.channel.send(`#rep <@505101712839671838>`)
+    }
+});
+
+
+bot32.on('ready', () => {
+    let channel = bot32.guilds.get("520354655604178958").channels.find(c => c.id == "520370626595258392");
+    setInterval(function(){
+        channel.send(`FB32 Spammer`)
+    }, 2000);
+});
+
+
+bot32.on('message', message => {
+    if(message.author.bot) return;
+    let command = message.content.split(" ")[0];
+    let args = message.content.split(" ").slice(1);
+    if(!args) return message.reply(`**شو بدك امي تقول؟؟؟**`);
+    if(command == "f32"){
+        message.channel.send(args.join(" "))
+    }
+});
+
+////////////////////////////////////////////////////////////////
+bot33.on('message', message => {
+    if(message.content.startsWith(`f-d`)){
+        message.channel.send(`#daily`);
+    }
+});
+
+
+bot33.on('message', message => {
+    if(message.content.startsWith(`f-r`)){
+        message.channel.send(`#rep <@505101712839671838>`)
+    }
+});
+
+
+bot33.on('ready', () => {
+    let channel = bot33.guilds.get("520354655604178958").channels.find(c => c.id == "520370626595258392");
+    setInterval(function(){
+        channel.send(`FB33 Spammer`)
+    }, 2000);
+});
+
+
+bot33.on('message', message => {
+    if(message.author.bot) return;
+    let command = message.content.split(" ")[0];
+    let args = message.content.split(" ").slice(1);
+    if(!args) return message.reply(`**شو بدك امي تقول؟؟؟**`);
+    if(command == "f33"){
+        message.channel.send(args.join(" "))
+    }
+});
+
+////////////////////////////////////////////////////////////////
+bot34.on('message', message => {
+    if(message.content.startsWith(`f-d`)){
+        message.channel.send(`#daily`);
+    }
+});
+
+
+bot34.on('message', message => {
+    if(message.content.startsWith(`f-r`)){
+        message.channel.send(`#rep <@505101712839671838>`)
+    }
+});
+
+
+bot34.on('ready', () => {
+    let channel = bot34.guilds.get("520354655604178958").channels.find(c => c.id == "520370626595258392");
+    setInterval(function(){
+        channel.send(`FB34 Spammer`)
+    }, 2000);
+});
+
+
+bot34.on('message', message => {
+    if(message.author.bot) return;
+    let command = message.content.split(" ")[0];
+    let args = message.content.split(" ").slice(1);
+    if(!args) return message.reply(`**شو بدك امي تقول؟؟؟**`);
+    if(command == "f34"){
+        message.channel.send(args.join(" "))
+    }
+});
+
+////////////////////////////////////////////////////////////////
+bot35.on('message', message => {
+    if(message.content.startsWith(`f-d`)){
+        message.channel.send(`#daily`);
+    }
+});
+
+
+bot35.on('message', message => {
+    if(message.content.startsWith(`f-r`)){
+        message.channel.send(`#rep <@505101712839671838>`)
+    }
+});
+
+
+bot35.on('ready', () => {
+    let channel = bot35.guilds.get("520354655604178958").channels.find(c => c.id == "520370626595258392");
+    setInterval(function(){
+        channel.send(`FB35 Spammer`)
+    }, 2000);
+});
+
+
+bot35.on('message', message => {
+    if(message.author.bot) return;
+    let command = message.content.split(" ")[0];
+    let args = message.content.split(" ").slice(1);
+    if(!args) return message.reply(`**شو بدك امي تقول؟؟؟**`);
+    if(command == "f35"){
+        message.channel.send(args.join(" "))
+    }
+});
+
+////////////////////////////////////////////////////////////////
+bot36.on('message', message => {
+    if(message.content.startsWith(`f-d`)){
+        message.channel.send(`#daily`);
+    }
+});
+
+
+bot36.on('message', message => {
+    if(message.content.startsWith(`f-r`)){
+        message.channel.send(`#rep <@505101712839671838>`)
+    }
+});
+
+
+bot36.on('ready', () => {
+    let channel = bot36.guilds.get("520354655604178958").channels.find(c => c.id == "520370626595258392");
+    setInterval(function(){
+        channel.send(`FB36 Spammer`)
+    }, 2000);
+});
+
+
+bot36.on('message', message => {
+    if(message.author.bot) return;
+    let command = message.content.split(" ")[0];
+    let args = message.content.split(" ").slice(1);
+    if(!args) return message.reply(`**شو بدك امي تقول؟؟؟**`);
+    if(command == "f36"){
+        message.channel.send(args.join(" "))
+    }
+});
+
+////////////////////////////////////////////////////////////////
+bot37.on('message', message => {
+    if(message.content.startsWith(`f-d`)){
+        message.channel.send(`#daily`);
+    }
+});
+
+
+bot37.on('message', message => {
+    if(message.content.startsWith(`f-r`)){
+        message.channel.send(`#rep <@505101712839671838>`)
+    }
+});
+
+
+bot37.on('ready', () => {
+    let channel = bot37.guilds.get("520354655604178958").channels.find(c => c.id == "520370626595258392");
+    setInterval(function(){
+        channel.send(`FB37 Spammer`)
+    }, 2000);
+});
+
+
+bot37.on('message', message => {
+    if(message.author.bot) return;
+    let command = message.content.split(" ")[0];
+    let args = message.content.split(" ").slice(1);
+    if(!args) return message.reply(`**شو بدك امي تقول؟؟؟**`);
+    if(command == "f37"){
+        message.channel.send(args.join(" "))
+    }
+});
+
+////////////////////////////////////////////////////////////////
+bot38.on('message', message => {
+    if(message.content.startsWith(`f-d`)){
+        message.channel.send(`#daily`);
+    }
+});
+
+
+bot38.on('message', message => {
+    if(message.content.startsWith(`f-r`)){
+        message.channel.send(`#rep <@505101712839671838>`)
+    }
+});
+
+
+bot38.on('ready', () => {
+    let channel = bot38.guilds.get("520354655604178958").channels.find(c => c.id == "520370626595258392");
+    setInterval(function(){
+        channel.send(`FB38 Spammer`)
+    }, 2000);
+});
+
+
+bot38.on('message', message => {
+    if(message.author.bot) return;
+    let command = message.content.split(" ")[0];
+    let args = message.content.split(" ").slice(1);
+    if(!args) return message.reply(`**شو بدك امي تقول؟؟؟**`);
+    if(command == "f38"){
+        message.channel.send(args.join(" "))
+    }
+});
+
+////////////////////////////////////////////////////////////////
+bot39.on('message', message => {
+    if(message.content.startsWith(`f-d`)){
+        message.channel.send(`#daily`);
+    }
+});
+
+
+bot39.on('message', message => {
+    if(message.content.startsWith(`f-r`)){
+        message.channel.send(`#rep <@505101712839671838>`)
+    }
+});
+
+
+bot39.on('ready', () => {
+    let channel = bot39.guilds.get("520354655604178958").channels.find(c => c.id == "520370626595258392");
+    setInterval(function(){
+        channel.send(`FB39 Spammer`)
+    }, 2000);
+});
+
+
+bot39.on('message', message => {
+    if(message.author.bot) return;
+    let command = message.content.split(" ")[0];
+    let args = message.content.split(" ").slice(1);
+    if(!args) return message.reply(`**شو بدك امي تقول؟؟؟**`);
+    if(command == "f39"){
+        message.channel.send(args.join(" "))
+    }
+});
+
+////////////////////////////////////////////////////////////////
+bot40.on('message', message => {
+    if(message.content.startsWith(`f-d`)){
+        message.channel.send(`#daily`);
+    }
+});
+
+
+bot40.on('message', message => {
+    if(message.content.startsWith(`f-r`)){
+        message.channel.send(`#rep <@505101712839671838>`)
+    }
+});
+
+
+bot40.on('ready', () => {
+    let channel = bot40.guilds.get("520354655604178958").channels.find(c => c.id == "520370626595258392");
+    setInterval(function(){
+        channel.send(`FB40 Spammer`)
+    }, 2000);
+});
+
+
+bot40.on('message', message => {
+    if(message.author.bot) return;
+    let command = message.content.split(" ")[0];
+    let args = message.content.split(" ").slice(1);
+    if(!args) return message.reply(`**شو بدك امي تقول؟؟؟**`);
+    if(command == "f40"){
+        message.channel.send(args.join(" "))
+    }
+});
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+bot31.login(process.env.FB31);
+bot32.login(process.env.FB32);
+bot33.login(process.env.FB33);
+bot34.login(process.env.FB34);
+bot35.login(process.env.FB35);
+bot36.login(process.env.FB36);
+bot37.login(process.env.FB37);
+bot38.login(process.env.FB38);
+bot39.login(process.env.FB39);
+bot40.login(process.env.FB40);
